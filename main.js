203cd96497eb9160dc0469c158f256a3a7d7ab93
Juan Ignacio Murphy <juanimurphy@gmail.com>
2024-04-29 23:10:43 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let check = prompt("¡Hola! ¿Querés que te asesore para que consigas un gran trabajo?")
+
+while ((check !== "si") && (check !== "sí")) {
+
+    if (check == "no") {
+        let secondCheck = confirm("Pero mirá que podés hacer muchísima guita eh... te interesa?")
+        if (secondCheck) {
+            alert("MUY BIEN")
+            check = "si"
+        }
+    }else {
+    check = prompt("Es una gran oportunidad. ¿Querés que te tire la posta?")
+    }
+}
+
+alert("ANOTATE EN CODER!!!")
